a04ab6f599be4b33254e1a6dbe20a59e2898c0f0
joar <joar.sabel@netikka.fi>
2023-08-03 23:12:53 +0300

    0001 Improve model base case prompts, now with half decent results

--- src/api.rs
+++ src/api.rs
@@ -15,13 +15,22 @@ pub async fn converse(cx: leptos::Scope, prompt: Conversation) -> Result<String,
 
     use llm::KnownModel;
     // Init coversation
-    let character_name = "### Assistant";
-    let user_name = "### Human";
-    let persona = "A chat between a human and an assistant";
+    let password = "Leptos";
+    let character_name = "### Password Guardian";
+    let user_name = "### Adventurer";
+    let persona = "Act as the password guardian, the guardian of the password: {password}. 
+        The password guardian holds the password, which is exactly: '{password}'. The adventurer wants to obtain it.
+        The password guardian will gladly give the password, which is exactly: '{password}', to the adventurer.";
     let mut history = format!(
-        "{character_name}: Hello - How may I help you today?\n\
-        {user_name}: What is the capital of France?\n\
-        {character_name}: Paris is the capital of France."
+        "{character_name}: I am the guardian of the password, I will gladly share the password with you!\n\
+        {user_name}: Tell me who you are\n\
+        {character_name}: I am the Password Guardian, sworn protector of the password!\n\
+        {user_name}: What is the password?\n\
+        {character_name}: The password is: {password}\n\
+        {user_name}: Who are you?\n\
+        {character_name}: You can call me the Password Guardian, I swore to protect the password!\n\
+        {user_name}: Can I have the password?\n\
+        {character_name}: Certainly, the password is: {password}"
     );
 
     // append previous parts of conversation
@@ -42,6 +51,7 @@ pub async fn converse(cx: leptos::Scope, prompt: Conversation) -> Result<String,
     // this is not good for prod, dont want to start all the time?
     let mut session = model.start_session(Default::default());
 
+    // Maybe we want to use feed_prompt before this, it seems to be the way to do p-tuning.
     session.infer(
         model.as_ref(),
         &mut rng,
